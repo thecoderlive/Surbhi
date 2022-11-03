ad80a0e7642ee2cf25aac527cf510a9731d7f7b5
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-11-03 12:40:24 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "f35ef658-59dc-42a0-98a2-d3b852b3bca3",
+    "home_feed": [
+        {
+            "id": "9a45fcc1-844f-403e-8046-f04b8673c742",
+            "artist_name": "seat",
+            "home_carousel": [
+                {
+                    "id": "a465aebf-8fed-44fa-88a1-bf5585f44b3a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/36.png"
+                },
+                {
+                    "id": "27ebcef1-ce7e-4899-82c3-9fbf3e360494",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/58.png"
+                },
+                {
+                    "id": "ce2f3e1e-dadf-4a22-ab2a-b1e67969a587",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/119.png"
+                },
+                {
+                    "id": "e961102c-14c3-41b4-ad59-72c202c5169c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/315.png"
+                },
+                {
+                    "id": "d49bbadc-7a17-4322-b969-ffb5e3406984",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/233.png"
+                }
+            ],
+            "art_caption": "west",
+            "art_description": "Age sell cover major happen size. His five million piece story pass card. Theory want job exactly term future leg check."
+        },
+        {
+            "id": "24fc483b-0790-42b6-aa58-b654272fe528",
+            "artist_name": "lot",
+            "home_carousel": [
+                {
+                    "id": "ecb04d20-2623-4784-8f94-6f8e8cf2761f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/162.png"
+                },
+                {
+                    "id": "572d8cc3-74bb-413e-97d4-b42b1278d1bd",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/54.png"
+                },
+                {
+                    "id": "67fb9203-9fae-4a89-ad29-6db737b88913",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/36.png"
+                },
+                {
+                    "id": "f362c613-2fee-4bc3-80e3-17d54a308838",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/103.png"
+                },
+                {
+                    "id": "d3aad126-cf06-4f3f-ae3c-df1fd7118012",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/340.png"
+                }
+            ],
+            "art_caption": "energy",
+            "art_description": "Sister old all no interesting. Professional whatever particularly sometimes already."
+        },
+        {
+            "id": "7f694d89-04d1-4441-8d10-d5dc7ad5a8e1",
+            "artist_name": "develop",
+            "home_carousel": [
+                {
+                    "id": "8662a829-e33b-441a-9705-29c20f6d9791",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/214.png"
+                },
+                {
+                    "id": "14a98783-804e-4b39-a258-359393a67985",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/288.png"
+                },
+                {
+                    "id": "39c78498-16d9-4e15-8067-f3c61055f320",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/297.png"
+                },
+                {
+                    "id": "eafb2da3-c66e-483a-888d-0300a4e1423b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/54.png"
+                },
+                {
+                    "id": "f2c65faf-0256-4aea-8e7a-a8bbeb83afb4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/59.png"
+                }
+            ],
+            "art_caption": "recently",
+            "art_description": "Too writer rise pick may. Run well and in. Join my go tough bill truth subject."
+        },
+        {
+            "id": "ba189014-28b2-4e7a-a862-c6a333edd5e6",
+            "artist_name": "form",
+            "home_carousel": [
+                {
+                    "id": "2971d118-545a-4922-9ecd-00756664900a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/99.png"
+                },
+                {
+                    "id": "65b3519c-5588-4543-a17a-7556a2b718ed",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/7.png"
+                },
+                {
+                    "id": "ff574431-13cd-4cbd-93f0-459f04886cb2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/15.png"
+                },
+                {
+                    "id": "5cf6bfd2-edc7-4fe1-9dcf-61d32e268f6b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/346.png"
+                },
+                {
+                    "id": "3abc4671-ec1e-4789-865a-c046b1e83ba5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/1.png"
+                }
+            ],
+            "art_caption": "floor",
+            "art_description": "Assume whose my let Mr respond environment politics. Generation purpose on professional time six."
+        },
+        {
+            "id": "61722683-bab9-4c72-b3ab-8b38c27c042b",
+            "artist_name": "find",
+            "home_carousel": [
+                {
+                    "id": "134b455b-c334-4eec-b10d-9eeb7e28121e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/55.png"
+                },
+                {
+                    "id": "3b47a35d-e516-4118-b3ac-17005e3b9304",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/174.png"
+                },
+                {
+                    "id": "c123c9fb-5cd2-497a-bc3e-7556f2f68bad",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/349.png"
+                },
+                {
+                    "id": "a7d212e2-4259-45ba-9322-ba4de73a95b9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/28.png"
+                },
+                {
+                    "id": "ef947da4-4eb3-4b2a-a6da-1955ecfda1cd",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/191.png"
+                }
+            ],
+            "art_caption": "agency",
+            "art_description": "Establish gun issue decision future fast. Yeah base magazine evidence scientist ask perform."
+        }
+    ]
+}
